8f81aacdfa9ba52f30e68df538cc126c7abaedd4
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-12-20 21:36:48 +0100

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+"""Empty class Square that defines a square"""
+
+
 class Square:
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+    """Empty class Square that defines a square"""
+    pass
